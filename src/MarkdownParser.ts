7ab95a3184debddc79d8e88bd41ffc511985f9ee
windsnow1025 <windsnow1025@gmail.com>
2025-06-06 06:52:05 -0700

    feat: upgrade dependencies

--- src/MarkdownParser.ts
+++ src/MarkdownParser.ts
@@ -5,10 +5,11 @@ import hljs from 'highlight.js';
 
 const marked = new Marked(
   markedHighlight({
+    emptyLangClass: 'hljs',
     langPrefix: 'hljs language-',
-    highlight(code, lang) {
+    highlight(code, lang, info) {
       const language = hljs.getLanguage(lang) ? lang : 'plaintext';
-      return hljs.highlight(code, {language}).value;
+      return hljs.highlight(code, { language }).value;
     }
   })
 );
