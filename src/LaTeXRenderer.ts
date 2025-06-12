b620732f7b42950becab4991c67465382ce79a7f
windsnow1025 <windsnow1025@gmail.com>
2025-06-12 10:48:29 -0700

    chore: katex config

--- src/LaTeXRenderer.ts
+++ src/LaTeXRenderer.ts
@@ -3,19 +3,17 @@ import 'katex/dist/katex.min.css';
 
 const katex_config = {
   delimiters: [
-    {left: '$$', right: '$$', display: true},
     {left: '$', right: '$', display: false},
+    {left: '$$', right: '$$', display: true},
     {left: '\\(', right: '\\)', display: false},
     {left: '\\[', right: '\\]', display: true}
   ],
 };
 
 /**
- * <div class="markdown-body" contenteditable="true">
- *   <p>
- *     \[e^{ix} = \cos(x) + i\sin(x)\]
- *   </p>
- * </div>
+ * <p>
+ *   \[e^{ix} = \cos(x) + i\sin(x)\]
+ * </p>
  */
 export function renderLaTeX(content_div: HTMLElement) {
   renderMathInElement(content_div, katex_config);
