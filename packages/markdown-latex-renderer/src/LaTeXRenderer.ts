ad0799ae965c35ef026921db005d63fdf863698e
windsnow1025 <windsnow1025@gmail.com>
2025-06-13 00:48:22 -0700

    feat: remove latex $ support

--- packages/markdown-latex-renderer/src/LaTeXRenderer.ts
+++ packages/markdown-latex-renderer/src/LaTeXRenderer.ts
@@ -1,20 +1,11 @@
 import renderMathInElement from "katex/contrib/auto-render";
 import 'katex/dist/katex.min.css';
 
-const katex_config = {
-  delimiters: [
-    {left: '$$', right: '$$', display: true},
-    {left: '$', right: '$', display: false},
-    {left: '\\(', right: '\\)', display: false},
-    {left: '\\[', right: '\\]', display: true}
-  ],
-};
-
 /**
  * <p>
  *   \[e^{ix} = \cos(x) + i\sin(x)\]
  * </p>
  */
 export function renderLaTeX(content_div: HTMLElement) {
-  renderMathInElement(content_div, katex_config);
+  renderMathInElement(content_div);
 }
