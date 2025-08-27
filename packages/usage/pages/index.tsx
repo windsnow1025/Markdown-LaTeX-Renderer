97b261ca872165b23a33222e67ea1d60a4a315df
windsnow1025 <windsnow1025@gmail.com>
2025-08-27 17:28:20 -0400

    test: mui theme

--- packages/usage/pages/index.tsx
+++ packages/usage/pages/index.tsx
@@ -1,30 +1,56 @@
 import '@/lib/global.css'
-import {useEffect, useRef, useState} from 'react';
-import {FormControlLabel, IconButton, Switch, Tooltip} from '@mui/material';
+import {useEffect, useState} from 'react';
+import {
+  createTheme, CssBaseline,
+  FormControlLabel,
+  IconButton,
+  Switch,
+  Theme,
+  ThemeProvider,
+  Tooltip
+} from '@mui/material';
 import DarkModeIcon from '@mui/icons-material/DarkMode';
 import LightModeIcon from '@mui/icons-material/LightMode';
 import VisibilityIcon from "@mui/icons-material/Visibility";
 import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
-// import {ThemeType} from "markdown-latex-renderer";
-import {ThemeType} from "markdown-latex-renderer/src";
 import TextContent from "@/app/TextContent";
 import {RawEditableState} from '@/lib/EditableState';
 import {testMarkdownContent} from "@/lib/TestMarkdownContent";
+// import {ThemeType} from "markdown-latex-renderer";
+import {ThemeType} from "markdown-latex-renderer/src";
+
+const lightMuiTheme = createTheme({
+  palette: {
+    mode: 'light',
+  },
+});
+
+const darkMuiTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
 
 export default function Home() {
   const sanitizeLevel = 0;
 
   const [theme, setTheme] = useState<ThemeType>(ThemeType.Dark);
+  const [muiTheme, setMuiTheme] = useState<Theme>(lightMuiTheme);
 
   const handleThemeChange = () => {
     setTheme(prevTheme => prevTheme === ThemeType.Light ? ThemeType.Dark : ThemeType.Light);
   };
 
+  useEffect(() => {
+    setMuiTheme(theme === ThemeType.Dark ? darkMuiTheme : lightMuiTheme);
+  }, [theme]);
+
   const [content, setContent] = useState(testMarkdownContent);
   const [showPreview, setShowPreview] = useState(true);
 
   return (
-    <>
+    <ThemeProvider theme={muiTheme}>
+      <CssBaseline />
       <div>
         <div className="flex items-center">
           <Tooltip title={showPreview ? "Edit Mode" : "Preview Mode"}>
@@ -58,6 +84,6 @@ export default function Home() {
           mode={theme}
         />
       </div>
-    </>
+    </ThemeProvider>
   );
 }
