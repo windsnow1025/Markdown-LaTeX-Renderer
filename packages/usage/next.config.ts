0ceb88a9bc30112c9be7eb5d1778bdba840f99d3
windsnow1025 <windsnow1025@gmail.com>
2026-03-27 11:00:08 -0400

    test: upgrade to next js 16

--- packages/usage/next.config.ts
+++ packages/usage/next.config.ts
@@ -2,7 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  transpilePackages: ['markdown-latex-renderer'],
+  transpilePackages: [
+    'markdown-latex-renderer',
+  ],
+  productionBrowserSourceMaps: true,
 };
 
 export default nextConfig;
