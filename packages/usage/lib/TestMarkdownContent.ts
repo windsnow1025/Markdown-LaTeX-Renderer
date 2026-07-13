e6f5ed14d49628ef54762053b363230392568def
windsnow1025 <windsnow1025@gmail.com>
2026-07-12 21:16:18 -0700

    test: update test markdown content

--- packages/usage/lib/TestMarkdownContent.ts
+++ packages/usage/lib/TestMarkdownContent.ts
@@ -1,6 +1,58 @@
 export const testMarkdownContent = `
 # Markdown LaTeX Renderer Demo
 
+## Code
+
+\`\`\`javascript
+function hello() {
+  console.log("Hello, world!");
+}
+\`\`\`
+
+## Table
+
+| Header 1 | Header 2 | Header 3 |
+|----------|----------|----------|
+| Cell 1,1 | Cell 1,2 | Cell 1,3 |
+| Cell 2,1 | Cell 2,2 | Cell 2,3 |
+| Cell 3,1 | Cell 3,2 | Cell 3,3 |
+
+## Link
+
+1. [google.com](https://www.google.com)
+
+## Parsing Test
+
+<div>&</div>
+
+\`<div>&</div>\`
+
+\`\`\`
+<div>&</div>
+\`\`\`
+
+\`\`\`html
+<div>&</div>
+\`\`\`
+
+<div>&</div>
+
+\`<div>&</div>\`
+
+\`\`\`
+<div><&></div>
+\`\`\`
+
+\`\`\`html
+<div><&></div>
+\`\`\`
+
+\`\`\`latex
+\\[
+e^{ix} = \\cos(x) + i\\sin(x)
+\\]
+\`\`\`
+
 ## Markdown Plugins
 
 ### Sub Plugin
@@ -32,56 +84,4 @@ $$
 ### \`$\` Inline Mode Unsupported
 
 $ e $, $ i $, $ x $, $ \\cos(x) $, $ \\sin(x) $
-
-## Code
-
-\`\`\`javascript
-function hello() {
-  console.log("Hello, world!");
-}
-\`\`\`
-
-## Table Example
-
-| Header 1 | Header 2 | Header 3 |
-|----------|----------|----------|
-| Cell 1,1 | Cell 1,2 | Cell 1,3 |
-| Cell 2,1 | Cell 2,2 | Cell 2,3 |
-| Cell 3,1 | Cell 3,2 | Cell 3,3 |
-
-## Link
-
-1. [google.com](https://www.google.com)
-
-## Parsing Test
-
-<div>&</div>
-
-\`<div>&</div>\`
-
-\`\`\`
-<div>&</div>
-\`\`\`
-
-\`\`\`html
-<div>&</div>
-\`\`\`
-
-<div>&amp;</div>
-
-\`<div>&amp;</div>\`
-
-\`\`\`
-<div>&lt;&amp;&gt;</div>
-\`\`\`
-
-\`\`\`html
-<div>&lt;&amp;&gt;</div>
-\`\`\`
-
-\`\`\`latex
-\\[
-e^{ix} = \\cos(x) + i\\sin(x)
-\\]
-\`\`\`
 `;
