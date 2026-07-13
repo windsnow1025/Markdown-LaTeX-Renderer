62d0bdf8dfa2346b7d45b5d452a20c306d4d3d2a
windsnow1025 <windsnow1025@gmail.com>
2026-07-12 21:35:58 -0700

    docs: add back transpile packages

--- packages/usage/next.config.ts
+++ packages/usage/next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  transpilePackages: [
+    'markdown-latex-renderer',
+  ],
   productionBrowserSourceMaps: true,
 };
 
